f3ba7f3d1fdef256aae0c708cbcf0aad0bffbcf3
Yakub-cyber <alhazurovyakub@gmail.com>
2025-08-15 01:40:46 +0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Замените repo-name на ИМЯ_РЕПОЗИТОРИЯ
 export default defineConfig({
 	plugins: [react()],
-	base: '/zvonoc2/', // например "/webrtc-audio/"
+	base: 'zvonoc2', // например "/webrtc-audio/"
 })
